1f8564cfed150307965446f92a909c8a0fa018c8
Daniel Macharia <danielhuios@gmail.com>
2022-10-22 04:15:19 +0300

    all test passed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
 
+    def initialize(title)
+        @title = title
+    end   
 
+    def properties (author, page_count, genre)
+       @author = author
+       @page_count = page_count
+       @genre = genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+book = Book.new("And Then There Were None")
+book.author = "Agatha Christie"
+book.page_count = 272
+book.genre = "Mystery"
+book2 = Book.new("The World According to Garp")
+book2.turn_page
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,30 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def properties(color, size, material, condition)
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
+
+shoe = Shoe.new("Adidas")
+shoe.brand = "Nike"
+shoe.color = "red"
+shoe.size = 9.5
+shoe.material = "suede"
+shoe.condition = "tattered"
+shoe.cobble
+puts shoe.inspect
+
